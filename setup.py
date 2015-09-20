a702a2a1cf867107e11deed31a1f69b401f16de5
Alex Michael <hi@alexmic.net>
2015-09-21 00:43:41 +0200

    Add README and setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as fd:
+    requirements = fd.readlines()
+
+
+with open('geotrie/version.py') as fd:
+    version = fd.read().split('=')[1].replace("'", '').strip()
+
+
+with open('README.md') as fd:
+    long_description = fd.read()
+
+
+setup(
+    name='geotrie-python',
+    version=version,
+    packages=find_packages(exclude=['tests']),
+    license='MIT',
+    author='Alex Michael',
+    author_email='hi@alexmic.net',
+    url='https://github.com/alexmic/geotrie-python',
+    description='Geospatial search using geohashes, backed by a trie.',
+    keywords=['geohash', 'trie', 'spatial', 'search'],
+    install_requires=requirements,
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries",
+    ]
+)
